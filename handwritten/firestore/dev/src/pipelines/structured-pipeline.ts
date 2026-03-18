b8519a34ad9214b22c835d2145da8ac554add814
Daniel La Rocque <dlarocque@google.com>
2026-03-18 10:56:46 -0400

    cleanup

--- handwritten/firestore/dev/src/pipelines/structured-pipeline.ts
+++ handwritten/firestore/dev/src/pipelines/structured-pipeline.ts
@@ -1,16 +1,19 @@
-// Copyright 2026 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * @license
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import {ProtoSerializable, Serializer} from '../serializer';
 import {google} from '../../protos/firestore_v1_proto_api';
@@ -86,10 +89,5 @@ export class StructuredPipeline
         this.optionsOverride,
       ),
     };
-  }
-
-  _validateUserData(ignoreUndefinedProperties: boolean): void {
-    // Structured pipeline options are primitive configurations or raw overrides.
-    // They do not contain Abstract Syntax Tree expressions that require deferred user data validation.
   }
 }
